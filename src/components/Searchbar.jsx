cc94e1a397f11b9a48ea2903b8ae1c769711ee24
Peacelawry <peacelawry@gmail.com>
2024-11-23 16:36:19 +0100

    search

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,22 +3,41 @@ import React from 'react'
 import { SearchVideos } from '@/utils/fetchData'
 import { useState } from 'react'
 
-const Searchbar = () => {
-  function update (){
-    setCount (count+1)
+const Searchbar = () => { 
+  const [ loading, setLoading ]=useState(false)
+  const [result , setResult]=useState([]);
+  const [ask , setAsk]=useState("");
+
+  const handleSearch = async (e) => {
+setLoading (true)
+e.preventDefault();
+const data = await  SearchVideos(`search/?query=${ask}`)   
+console.log(data)
+setResult(data)
+setLoading(false)
   }
-const [count , setCount] = useState(0)
+
+
+//   function update (){
+//     setCount (count+1)
+//   }
+// const [count , setCount] = useState(0)
 
 
   return (
     <div>
-      <form><input type="text" placeholder='search...' />
+      <form onSubmit={handleSearch}>
+        <input type="text" placeholder='search...'
+        value={ask}
+        onChange={(e) => setAsk(e.target.value)} 
+         />
       <button>
         search
       </button>
       </form>
-<h1>{count}</h1>
-<button onClick={update}>update count</button>
+      {
+        loading ? <p>loading...</p> : null
+      }
     </div>
   )
 }
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,13 +1,13 @@
 const BASE_URL =`https://youtube-v2.p.rapidapi.com`
 
- export const SearchVideos =(query) =>{
-    const res = fetch(`${BASE-URL}/${query}`, {
+ export const SearchVideos = async (param) =>{
+    const res = await fetch(`${BASE_URL}/${param}`, {
         method: "GET" ,
         headers: {
-'x-rapidapi-key': '041ba30eb2msh9d5992fe0703020p14743ajsn7a1fec72db0f',
+        'x-rapidapi-key': '041ba30eb2msh9d5992fe0703020p14743ajsn7a1fec72db0f',
 		'x-rapidapi-host': 'youtube-v2.p.rapidapi.com'
         }
     })
-    const result =res.json();
+    const result = await res.json();
     return result;
 }
